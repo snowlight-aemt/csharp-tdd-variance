079cf7cac02b7b6edb6dd4fab75974b2bdfb0cd2
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2023-01-14 00:06:56 +0900

    분산 기능 추가

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        double[] s = new double[args.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            s[i] = double.Parse(args[i]);
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            sum += s[i];
+        }
+
+        double mean = sum / s.Length;
+        double sumOfSquares = 0.0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            sumOfSquares += (s[i] - mean) * (s[i] - mean);
+        }
+
+        double variance = sumOfSquares / (s.Length - 1);
+
+        Console.WriteLine($"분산: {variance}");
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+## Variance
+
+
+```shell
+dotnet new console --use-program-main -n Variance
+
+dotnet new console -n Variance
+# See https://aka.ms/new-console-template for more information
+```
+
