2ec888b3bdc1ade550811c891b292143fa059cce
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2023-01-14 14:13:12 +0900

    리펙토링 1

--- Program.cs
+++ Program.cs
@@ -4,39 +4,56 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        if (args.Length == 0) 
+        Console.WriteLine(args.Length switch
         {
-            Console.WriteLine("데이터가 입력되지 않았습니다.");
-            return;
-        }
-        else if (args.Length == 1)
+            0 => "데이터가 입력되지 않았습니다.",
+            1 => "2개 이상의 데이터를 입력하세요.",
+            _ => GetVerianceOutput(args),
+        });
+    }
+
+    private static string GetVerianceOutput(string[] args)
+    {
+        double[] source = ParseArguments(args);
+        double mean = CalculateMean(source);
+        double sumOfSquares = CalcuateSumOfSquares(source, mean);
+
+        double variance = sumOfSquares / (source.Length - 1);
+        string output = $"분산: {variance}";
+        return output;
+    }
+
+    private static double CalcuateSumOfSquares(double[] source, double mean)
+    {
+        double sumOfSquares = 0.0;
+        for (int i = 0; i < source.Length; i++)
         {
-            Console.WriteLine("2개 이상의 데이터를 입력하세요.");
-            return;
+            sumOfSquares += (source[i] - mean) * (source[i] - mean);
         }
 
+        return sumOfSquares;
+    }
+
+    private static double CalculateMean(double[] source)
+    {
+        double sum = 0.0;
+        for (int i = 0; i < source.Length; i++)
+        {
+            sum += source[i];
+        }
+
+        double mean = sum / source.Length;
+        return mean;
+    }
+
+    private static double[] ParseArguments(string[] args)
+    {
         double[] s = new double[args.Length];
         for (int i = 0; i < s.Length; i++)
         {
             s[i] = double.Parse(args[i]);
         }
 
-        double sum = 0.0;
-        for (int i = 0; i < s.Length; i++)
-        {
-            sum += s[i];
-        }
-
-        double mean = sum / s.Length;
-        double sumOfSquares = 0.0;
-        for (int i = 0; i < s.Length; i++)
-        {
-            sumOfSquares += (s[i] - mean) * (s[i] - mean);
-        }
-
-        double variance = sumOfSquares / (s.Length - 1);
-
-        Console.WriteLine($"분산: {variance}");
-
+        return s;
     }
 }
